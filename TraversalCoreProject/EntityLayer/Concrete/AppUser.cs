f844bd154fd6753516d0752753f8d1623e4d3525
Onur METİN <onurmetin95@gmail.com>
2024-10-21 16:12:39 +0300

    identity packages added

---
+++ TraversalCoreProject/EntityLayer/Concrete/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace EntityLayer.Concrete
+{
+	public class AppUser : IdentityUser<int>
+	{
+		public string ImageURL { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set;  }
+		public string Gender { get; set; }
+	}
+}
+
