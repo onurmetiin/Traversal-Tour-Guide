a2ec3d47a9acf4ba68cee27394eb6e1347e3492f
Onur METİN <onurmetin95@gmail.com>
2024-10-22 15:28:26 +0300

    until 36 login succedd

---
+++ TraversalCoreProject/TraversalCoreProject/Areas/Member/Models/UserEditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TraversalCoreProject.Areas.Member.Models
+{
+	public class UserEditViewModel
+	{
+		public string name { get; set; }
+		public string surname { get; set; }
+		public string password { get; set; }
+		public string confirmPassword { get; set; }
+		public string phoneNumber { get; set; }
+		public string mail { get; set; }
+        public string imageURL { get; set; }
+	}
+}
+
---
+++ TraversalCoreProject/TraversalCoreProject/Models/UserSignInViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProject.Models
+{
+	public class UserSignInViewModel
+	{
+		[Required(ErrorMessage = "Lütfen Kullanıcı Adını Giriniz!")]
+		public string Username { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifrenizi Giriniz!")]
+        public string Password { get; set; }
+    }
+}
+
