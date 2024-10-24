e0fce54910c84afcabaacdaec070c527a8c78726
Onur METİN <onurmetin95@gmail.com>
2024-10-24 10:56:37 +0300

    until 47 platform setting

---
+++ TraversalCoreProject/TraversalCoreProject/ViewComponents/MemberDashboard/_GuideList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.MemberDashboard
+{
+	public class _GuideList : ViewComponent
+	{
+		GuideManager guideManager = new GuideManager(new EfGuideDal());
+
+		public IViewComponentResult Invoke()
+		{
+			var guideList = guideManager.TGetList();
+			return View(guideList);
+		}
+	}
+}
+
