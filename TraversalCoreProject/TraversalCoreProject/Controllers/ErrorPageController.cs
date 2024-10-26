0849e8740c8b7256dc33d463c5fc0c54795489c1
Onur METİN <onurmetin95@gmail.com>
2024-10-26 12:01:42 +0300

    error page added

---
+++ TraversalCoreProject/TraversalCoreProject/Controllers/ErrorPageController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace TraversalCoreProject.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        public IActionResult Error404(int code)
+        {
+
+            return View();
+        }
+    }
+}
+
--- TraversalCoreProject/TraversalCoreProject/Program.cs
+++ TraversalCoreProject/TraversalCoreProject/Program.cs
@@ -55,6 +55,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseStatusCodePagesWithReExecute("/ErrorPage/Error404", "?code={0}");
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
