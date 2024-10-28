bbf2a4043e7d9beaa68ae8321263dffe44bc3ce0
Onur METİN <onurmetin95@gmail.com>
2024-10-28 09:03:59 +0300

    until 58

--- TraversalCoreProject/BusinessLayer/Container/CustomizeExtension.cs
+++ TraversalCoreProject/BusinessLayer/Container/CustomizeExtension.cs
@@ -22,6 +22,9 @@ namespace BusinessLayer.Container
 
             services.AddScoped<IReservationService, ReservationManager>();
             services.AddScoped<IReservationDal, EfReservationDal>();
+
+            services.AddScoped<IGuideService, GuideManager>();
+            services.AddScoped<IGuideDal, EfGuideDal>();
         }
 	}
 }
---
+++ TraversalCoreProject/TraversalCoreProject/Areas/Admin/Controllers/GuideController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GuideController : Controller
+    {
+        private readonly IGuideService _guideService;
+
+        public GuideController(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+
+        public IActionResult Index()
+        {
+            var guideList = _guideService.TGetList();
+            return View(guideList);
+        }
+
+        [HttpGet]
+        public IActionResult AddGuide()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddGuide(Guide newGuide)
+        {
+            _guideService.TAdd(newGuide);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditGuide(int id)
+        {
+            var editedGuide = _guideService.TGetByID(id);
+            _guideService.TUpdate(editedGuide);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult EditGuide(Guide guide)
+        {
+            _guideService.TUpdate(guide);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ChangeStatusToActive(int id)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ChangeStatusToInactive(int id)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
+
